33ed12694ab65336b7923363e15b4b0917e8851e
wooohyerim <hyerim3414@gmail.com>
2024-10-10 14:35:43 +0900

    ADD : markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 function App() {
   return (
-    <section id="tables">
-      <h2>Poke API</h2>
-      <div className="overflow-auto">
-        <table className="striped">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Heading</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Cell</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </section>
+    <>
+      <section id="tables">
+        <h2>Poke API</h2>
+        <div className="overflow-auto">
+          <table className="striped">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Heading</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Cell</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </section>
+      <button type="button">-</button>
+      <span>1</span>
+      <button type="button">+</button>
+    </>
   )
 }
 
