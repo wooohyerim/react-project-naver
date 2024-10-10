c3024c2a4129373c364554dcae178190c9bef1c3
wooohyerim <hyerim3414@gmail.com>
2024-10-10 18:49:55 +0900

    params query string 설정

---
+++ src/JotailWithRRD.tsx
@@ -0,0 +1,81 @@
+import { Suspense } from 'react'
+import { useSearchParams } from 'react-router-dom'
+
+import { atom, useAtom, useAtomValue } from 'jotai'
+import { atomFamily } from 'jotai/utils'
+
+import { getPokeApi } from './api/pokeApi'
+
+const listAtomFamily = atomFamily((page: number) => {
+  return atom(async () => {
+    const root = await getPokeApi(page)
+    return root.results
+  })
+})
+
+const Table = () => {
+  const [params] = useSearchParams()
+  const page = Number(params.get('page')) || 0
+  const list = useAtomValue(listAtomFamily(page))
+
+  return (
+    <table className="striped">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Heading</th>
+        </tr>
+      </thead>
+      <tbody>
+        {list.map((item, index) => {
+          return (
+            <tr key={item.name}>
+              <th scope="row">{index}</th>
+              <td>{item.name}</td>
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
+}
+
+const JotaiWithRRD = () => {
+  const [params, setParams] = useSearchParams()
+
+  const page = Number(params.get('page')) || 0
+
+  return (
+    <>
+      <section id="tables">
+        <h2>Poke API</h2>
+        <div className="overflow-auto">
+          <Suspense fallback="로딩중">
+            <Table />
+          </Suspense>
+        </div>
+      </section>
+      <button
+        type="button"
+        onClick={() => {
+          setParams({
+            page: `${page - 1}`
+          })
+        }}
+      >
+        -
+      </button>
+      <span>{page}</span>
+      <button
+        type="button"
+        onClick={() => {
+          setParams({ page: `${page + 1}` })
+        }}
+      >
+        +
+      </button>
+    </>
+  )
+}
+
+export default JotaiWithRRD
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App.tsx'
+import JotaiWithRRD from './JotailWithRRD.tsx'
 import JotaiOnly from './JotaiOnly'
 
 import './index.css'
@@ -26,6 +27,9 @@ const NavOutlet = () => {
         <li>
           <NavLink to="jotai-only">jotai 페이징</NavLink>
         </li>
+        <li>
+          <NavLink to="jotai-with-rrd">jotai + react-router-dom</NavLink>
+        </li>
       </ul>
       <Outlet />
     </>
@@ -37,6 +41,7 @@ const router = createBrowserRouter(
     <Route element={<NavOutlet />}>
       <Route path="/classic" element={<App />} />
       <Route path="/jotai-only" element={<JotaiOnly />} />
+      <Route path="/jotai-with-rrd" element={<JotaiWithRRD />} />
       {/* 
       트랩 
       인증 페이지에 주로 사용*/}
