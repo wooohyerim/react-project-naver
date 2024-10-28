77016ee0bd8acb0f81fa4880916a961d1ed43b19
wooohyerim <hyerim3414@gmail.com>
2024-10-28 10:40:24 +0900

    Updates

---
+++ assets/Table.stories-DB_Lql0v.js
@@ -0,0 +1,14 @@
+import{T as d}from"./Table-CJbWRLYx.js";import"./jsx-runtime-QvZ8i92b.js";import"./index-uubelm5h.js";const g={component:d},a={args:{columns:["#","Heading"],data:[]}},r={args:{columns:["#","Heading"],data:[{"#":"1",Heading:"2"}]}};var e,s,n;a.parameters={...a.parameters,docs:{...(e=a.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    columns: ['#', 'Heading'],
+    data: []
+  }
+}`,...(n=(s=a.parameters)==null?void 0:s.docs)==null?void 0:n.source}}};var o,t,c;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    columns: ['#', 'Heading'],
+    data: [{
+      '#': '1',
+      Heading: '2'
+    }]
+  }
+}`,...(c=(t=r.parameters)==null?void 0:t.docs)==null?void 0:c.source}}};const u=["데이터가_없을때","데이터가_있을때"];export{u as __namedExportsOrder,g as default,a as 데이터가_없을때,r as 데이터가_있을때};
